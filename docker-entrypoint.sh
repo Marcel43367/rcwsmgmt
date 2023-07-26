e8e7cbc2de147ccd6d4323b034b1b09279ef6953
Christopher Schenk <christopher@cschenk.net>
2023-07-26 19:16:10 +0200

    Add log output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,6 @@ python manage.py compilemessages
 # Start Gunicorn processes
 exec gunicorn rcwsmgmt.wsgi:application \
         --bind 0.0.0.0:8000 \
-        --workers 3
+        --workers 3 \
+	--access-logfile /var/log/gunicorn-access.log \
+	--error-logfile /var/log/gunicorn-error.log
