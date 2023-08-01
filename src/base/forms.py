b19fc18176501780da19abe100b6a33a8add6b30
Christopher Schenk <christopher@cschenk.net>
2023-08-01 19:32:50 +0200

    Add max length for Feedback subject

--- src/base/forms.py
+++ src/base/forms.py
@@ -1,7 +1,7 @@
 from django.forms import Form, Textarea, CharField, BooleanField
 
 class WorkshopFeedbackForm(Form):
-	subject = CharField(required=True)
+	subject = CharField(required=True, max_length=64)
 	message = CharField(widget=Textarea, required=True)
 
 class WorkshopAnnotateForm(Form):
