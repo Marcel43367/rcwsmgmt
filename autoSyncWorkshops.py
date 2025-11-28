18acf2583f0ae052b489d2cac9aa22e07b32551f
Marcel43367 <50874321+Marcel43367@users.noreply.github.com>
2025-11-28 12:40:57 +0100

    Bugfix in autoSyncWorkshops
    wrap sync_workshops into try_catch block

--- autoSyncWorkshops.py
+++ autoSyncWorkshops.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """Auto sync workshops from pretix to database."""
 import os
-import sys
 import datetime
 import time
+import logging
 
 from django.core.management import execute_from_command_line
 
@@ -11,11 +11,13 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rcwsmgmt.settings")
     while True:
-            now = datetime.datetime.now()
-            if now.minute == 0 and now.second == 0:
+        now = datetime.datetime.now()
+        if now.minute == 0 and now.second == 0:
+            try:
                 execute_from_command_line(['manage.py', 'sync_workshops'])
-            time.sleep(1)
-
+            except Exception as e:
+                logging.error(f"An error occurred while syncing workshops: {e}")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
